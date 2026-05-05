32abc4d62728114119401b753d7cf1198382c721
Amit <programmer79.dynamicdreamz@gmail.com>
2026-05-05 14:42:58 +0530

    revert testing changes

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -74,11 +74,11 @@ const Footer = ({ className, whiteFooter }) => {
                   <Grid item xs={6}>
                     <S.Button
                       fullWidth
-                      className="light footer-item"
+                      className="light"
                       href="/contact/"
                       onClick={() => trackGetInTouchClick()}
                     >
-                      Get in touch hello
+                      Get in touch
                     </S.Button>
                   </Grid>
                   <Grid item xs={6}>
