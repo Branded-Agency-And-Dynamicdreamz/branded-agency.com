d52809d79c63bb8cf1d00b4f78e6356d17ab1040
Amit <programmer79.dynamicdreamz@gmail.com>
2026-05-06 10:45:44 +0530

    revert weglot changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,17 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
 import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import Header from "../components/header/header.component"
 import { AppContainer } from "../styles/app.styles"
 import Footer from "./footer/footer.component"
 import { isBrowser, setSessionStorageItem } from "../utils/utils"
+import { Element } from "react-scroll"
 import { useLocation } from "@reach/router"
 
 import { IS_PRODUCTION } from "../utils/constants"
@@ -23,131 +31,92 @@ const Layout = ({
 }) => {
   const location = useLocation()
   const isHome = location.pathname === "/"
-
   const [gtmLoaded, setGtmLoaded] = useState(false)
-
+  const [weglotLoaded, setWeglotLoaded] = useState(false)
   useEffect(() => {
     if (isBrowser) {
       const urlParams = new URLSearchParams(window.location.search)
-
       const utm_source = urlParams.get("utm_source")
       const utm_medium = urlParams.get("utm_medium")
       const utm_campaign = urlParams.get("utm_campaign")
-
       if (utm_source) {
         setSessionStorageItem("utm_source", utm_source)
       }
-
       if (utm_medium) {
         setSessionStorageItem("utm_medium", utm_medium)
       }
-
       if (utm_campaign) {
         setSessionStorageItem("utm_campaign", utm_campaign)
       }
     }
   }, [])
-
   return (
     <>
       {IS_PRODUCTION && (
         <>
-          {/* Osano */}
           <Script src="https://cmp.osano.com/AzyrZXU6jDIam3dtK/24c3e19f-6f8e-4661-b8e8-6862487b22f4/osano.js" />
-
-          {/* Navu */}
-          <Script src="https://embed.navu.co/boot.js" />
-
-          {/* Google Analytics */}
+          <Script src={"https://embed.navu.co/boot.js"} />
           <Script
             src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.GATSBY_GA_TRACKING_ID}`}
             onLoad={() => setGtmLoaded(true)}
           />
-
           {gtmLoaded && (
             <Script id="gtag-config">
-              {`
-                window.dataLayer = window.dataLayer || [];
-
-                function gtag(){
-                  dataLayer.push(arguments);
-                }
-
-                gtag('js', new Date());
-
-                gtag('config', 'G-${process.env.GATSBY_GA_TRACKING_ID}');
-                gtag('config', 'AW-11137911214');
+              {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+          
+              gtag('config', 'G-${process.env.GATSBY_GA_TRACKING_ID}');
+              gtag('config', 'AW-11137911214');
               `}
             </Script>
           )}
-
-          {/* Creative Perceptive */}
-          <Script src="https://www.creativeperceptive.com/js/799500.js" />
-
-          {/* Weglot */}
+          <Script src={`https://www.creativeperceptive.com/js/799500.js`} />
           <Script
+            type="text/javascript"
             src="https://cdn.weglot.com/weglot.min.js"
-            strategy="afterInteractive"
-            onLoad={() => {
-              if (window.Weglot) {
-                window.Weglot.initialize({
-                  api_key: "wg_99db751438638e0ddcb5df66e92cf6b73",
-                })
-              }
-            }}
+            onLoad={() => setWeglotLoaded(true)}
           />
-
-          {/* LinkedIn */}
+          {weglotLoaded && (
+            <Script id="second-unique-id">
+              {`Weglot.initialize({
+                api_key: 'wg_d54cea8743b81f7b9b07dcf9242cedba8'
+            });`}
+            </Script>
+          )}
           {gtmLoaded && (
             <>
               <Script
                 id="linkedin"
                 dangerouslySetInnerHTML={{
-                  __html: `
-                    _linkedin_partner_id = "4742298";
-
-                    window._linkedin_data_partner_ids =
-                      window._linkedin_data_partner_ids || [];
-
-                    window._linkedin_data_partner_ids.push(
-                      _linkedin_partner_id
-                    );
-                  `,
+                  __html: `_linkedin_partner_id = "4742298";
+                  window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+                  window._linkedin_data_partner_ids.push(_linkedin_partner_id);`,
                 }}
               />
-
               <Script
                 id="linkedin-config"
                 dangerouslySetInnerHTML={{
-                  __html: `
-                    (function(l) {
-                      if (!l) {
-                        window.lintrk = function(a, b) {
-                          window.lintrk.q.push([a, b])
-                        };
-
-                        window.lintrk.q = [];
-                      }
-
-                      var s = document.getElementsByTagName("script")[0];
-
-                      var b = document.createElement("script");
-
-                      b.type = "text/javascript";
-                      b.async = true;
-                      b.src =
-                        "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-
-                      s.parentNode.insertBefore(b, s);
-                    })(window.lintrk);
-                  `,
+                  __html: `(function(l) {
+                        if (!l) {
+                          window.lintrk = function(a, b) {
+                            window.lintrk.q.push([a, b])
+                          };
+                          window.lintrk.q = []
+                        }
+                        var s = document.getElementsByTagName("script")[0];
+                        var b = document.createElement("script");
+                        b.type = "text/javascript";
+                        b.async = true;
+                        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                        s.parentNode.insertBefore(b, s);
+                      })(window.lintrk);`,
                 }}
               />
             </>
           )}
         </>
       )}
-
       {!hideHeader && (
         <Header
           hideItems={hideHeaderItems}
@@ -157,20 +126,17 @@ const Layout = ({
           whiteHeader={whiteHeader}
         />
       )}
-
       <AppContainer
         id="top"
         className={`
-          ${isTransparentHeader ? "transparent" : ""}
-          ${hideHeader ? "hideHeader" : ""}
-          ${hideHeaderOnScroll && !whiteHeader ? "transparentHeader" : ""}
-          ${whitePinSpacer ? "whitePinSpacer" : ""}
-          ${isHome ? "home" : ""}
-        `}
+        ${isTransparentHeader ? "transparent" : ""} 
+        ${hideHeader ? "hideHeader" : ""} 
+        ${hideHeaderOnScroll && !whiteHeader ? "transparentHeader" : ""}
+        ${whitePinSpacer ? "whitePinSpacer" : ""}
+        ${isHome ? "home" : ""}`}
       >
         {children}
       </AppContainer>
-
       {!hideFooter && <Footer whiteFooter={whiteFooter} />}
     </>
   )
